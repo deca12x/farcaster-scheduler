efe6ddde9e754779a260edbe9a8b340e8d6b07f9
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-07-13 01:18:24 +0200

    neynar auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,57 @@
+"use client";
+
 import Image from "next/image";
+// import { useState, useRef } from "react";
+import { NeynarAuthButton, useNeynarContext } from "@neynar/react";
 
 export default function Home() {
+  const { user } = useNeynarContext();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const inputFile = useRef<HTMLInputElement>(null);
+
+  // const handleCastClick = async () => {
+  //   if (user) {
+  //     setIsLoading(true);
+
+  //     const file = inputFile.current?.files?.[0];
+  //     const formData = new FormData();
+  //     formData.append("user", JSON.stringify(user));
+  //     if (file) {
+  //       formData.append("file", file);
+  //     }
+
+  //     try {
+  //       const res = await fetch("/api/route", {
+  //         method: "POST",
+  //         body: formData,
+  //       });
+  //       const data = await res.json();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error("Error:", error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   } else {
+  //     console.error("User is not authenticated");
+  //   }
+  // };
+
+  // ADD BELOW
+  // <input type="file" ref={inputFile} />
+  // <button
+  //   onClick={handleCastClick}
+  //   className="bg-blue-500 text-white px-4 py-2 rounded-md"
+  //   disabled={isLoading}
+  // >
+  //   {isLoading ? "Posting..." : "Post Cast"}
+  // </button>
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          farcaster scheduler
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -40,71 +85,42 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <NeynarAuthButton />
+
+        {user && (
+          <>
+            <div className="flex flex-col gap-4 w-96 p-4 rounded-md shadow-md">
+              <div className="flex items-center gap-4">
+                {user.pfp_url && (
+                  <Image
+                    src={user.pfp_url}
+                    width={40}
+                    height={40}
+                    alt="User Profile Picture"
+                    className="rounded-full"
+                  />
+                )}
+                <p className="text-lg font-semibold">{user?.display_name}</p>
+                <p className="text-lg font-semibold">{user?.fid}</p>
+              </div>
+            </div>
+          </>
+        )}
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/deca12x/qcast-scheduler"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Github repo{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            q your casts
           </p>
         </a>
       </div>
