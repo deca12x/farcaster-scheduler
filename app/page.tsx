e96feda12e7d272a749c8356bc21af0de3b963c0
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-07-13 23:05:43 +0200

    main page styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { NeynarAuthButton, useNeynarContext } from "@neynar/react";
 import { DynamicWidget } from "@/lib/dynamic";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "daisyui/dist/full.css";
 
 export default function Home() {
@@ -15,8 +15,14 @@ export default function Home() {
   const [publishTime, setPublishTime] = useState<string>("");
   const [publishDate, setPublishDate] = useState<string>("");
 
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFile(e.target.files ? e.target.files[0] : null);
+  };
+
+  const handleAddImageClick = () => {
+    fileInputRef.current?.click();
   };
 
   const handleCastClick = async () => {
@@ -53,33 +59,36 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-row bg-base-200">
       {/* Sidebar */}
-      <aside className="w-1/4 bg-base-100 p-4 shadow-lg">
-        <div className="flex flex-col items-center">
-          <div className="mb-4">
-            <Image src="/logo.svg" alt="Logo" width={100} height={50} />
-          </div>
-          <button className="btn btn-primary mb-6">Add Account</button>
-          {/* Accounts List */}
-          <div className="flex flex-col gap-4">
-            {[1, 2, 3, 4].map((index) => (
-              <div
-                key={index}
-                className="flex items-center gap-4 p-2 bg-base-100 rounded shadow"
-              >
-                <div className="avatar">
-                  <div className="w-10 h-10 rounded-full bg-neutral-focus"></div>
-                </div>
-                <p className="text-lg font-semibold">Nome</p>
+      <aside className="w-1/5 bg-base-100 p-4 shadow-lg flex flex-col items-center gap-4">
+        <div className="mb-4">
+          <Image src="/logo.svg" alt="Logo" width={100} height={50} />
+        </div>
+        <button className="btn btn-primary mb-6">Add Account</button>
+        {/* Added Widgets */}
+        <div className="w-full flex flex-col gap-4">
+          <DynamicWidget className="p-2 rounded shadow bg-base-100" />
+          <NeynarAuthButton className="p-2 rounded shadow bg-base-100" />
+        </div>
+        {/* Accounts List */}
+        <div className="flex flex-col gap-4 w-full">
+          {[1, 2, 3, 4].map((index) => (
+            <div
+              key={index}
+              className="flex items-center gap-4 p-2 bg-base-100 rounded shadow"
+            >
+              <div className="avatar">
+                <div className="w-10 h-10 rounded-full bg-neutral-focus"></div>
               </div>
-            ))}
-          </div>
+              <p className="text-lg font-semibold">Nome</p>
+            </div>
+          ))}
         </div>
       </aside>
 
       {/* Main Content */}
-      <section className="w-3/4 p-6">
+      <section className="w-4/5 p-6">
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-semibold">Schedule a Cast</h1>
+          <h1 className="text-xl font-semibold">New Cast</h1>
           <a href="#" className="link link-primary">
             Analytics
           </a>
@@ -92,13 +101,8 @@ export default function Home() {
               value={castText}
               onChange={(e) => setCastText(e.target.value)}
             ></textarea>
-            <input
-              type="file"
-              onChange={handleFileChange}
-              className="input input-bordered w-full"
-            />
             <div className="flex gap-4">
-              <div className="w-1/3">
+              <div className="flex-1">
                 <label className="block text-sm font-medium">
                   Publish Date
                 </label>
@@ -109,7 +113,7 @@ export default function Home() {
                   className="input input-bordered w-full"
                 />
               </div>
-              <div className="w-1/3">
+              <div className="flex-1">
                 <label className="block text-sm font-medium">
                   Publish Time
                 </label>
@@ -120,32 +124,57 @@ export default function Home() {
                   className="input input-bordered w-full"
                 />
               </div>
-              <input
-                type="text"
-                value={channel}
-                onChange={(e) => setChannel(e.target.value)}
-                placeholder="Channel"
-                className="input input-bordered w-1/3"
-              />
+              <div className="flex-1">
+                <label className="block text-sm font-medium">Channel</label>
+                <input
+                  type="text"
+                  value={channel}
+                  onChange={(e) => setChannel(e.target.value)}
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="flex-1 flex items-end">
+                <button
+                  className="btn btn-outline w-full"
+                  onClick={handleAddImageClick}
+                >
+                  Add Image
+                </button>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+                  className="hidden"
+                />
+              </div>
             </div>
             <button
               onClick={handleCastClick}
-              className="btn btn-primary"
+              className="btn btn-primary mt-4"
               disabled={isLoading}
             >
               {isLoading ? "Scheduling..." : "Schedule"}
             </button>
           </div>
         </div>
-        <div className="mt-6 bg-base-100 p-4 rounded shadow-md">
-          <p>Post cast txt</p>
-          <div className="flex justify-between mt-4">
-            <div className="avatar">
-              <div className="w-10 h-10 rounded-full bg-neutral-focus"></div>
+        <h1 className="text-xl font-semibold">Scheduled Casts</h1>
+        <div className="mt-12 bg-base-100 p-4 rounded shadow-md">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <div className="avatar">
+                <div className="w-10 h-10 rounded-full bg-neutral-focus"></div>
+              </div>
+              <div className="ml-4">
+                <p className="font-semibold">Post cast txt</p>
+                <div className="text-gray-600">
+                  <p>DD/MM/YY</p>
+                  <p>00:00</p>
+                </div>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <p className="text-gray-600">DD/MM/YY</p>
-              <p className="text-gray-600">00:00</p>
+            <div className="flex gap-2">
+              <button className="btn btn-secondary btn-sm">Edit Cast</button>
+              <button className="btn btn-error btn-sm">Delete Cast</button>
             </div>
           </div>
         </div>
