12d5bb2f3c1a3f16161ebc64b5c051267ee8c125
feka7 <feka7@hotmail.it>
2024-07-13 10:32:38 +0200

    add providers component, update next config with remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.imgur.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
