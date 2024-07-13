eecae63b1c7c52ecfeee0928a030b119a9fc122f
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-07-13 21:33:38 +0200

    fe structure update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@
 import Image from "next/image";
 import { NeynarAuthButton, useNeynarContext } from "@neynar/react";
 import { DynamicWidget } from "@/lib/dynamic";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import "daisyui/dist/full.css";
 
 export default function Home() {
   const { user } = useNeynarContext();
@@ -11,6 +12,8 @@ export default function Home() {
   const [file, setFile] = useState<File | null>(null);
   const [castText, setCastText] = useState<string>("");
   const [channel, setChannel] = useState<string>("");
+  const [publishTime, setPublishTime] = useState<string>("");
+  const [publishDate, setPublishDate] = useState<string>("");
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFile(e.target.files ? e.target.files[0] : null);
@@ -48,101 +51,110 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          farcaster scheduler
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <main className="flex min-h-screen flex-row">
+      {/* Sidebar */}
+      <aside className="w-1/4 bg-gray-100 p-4">
+        <div className="flex flex-col items-center">
+          <div className="mb-4">
+            <Image src="/logo.svg" alt="Logo" width={100} height={50} />
+          </div>
+          <button className="flex items-center justify-center mb-6 bg-purple-600 text-white py-2 px-4 rounded">
+            Add Account
+          </button>
+          {/* Accounts List */}
+          <div className="flex flex-col gap-4">
+            {[1, 2, 3, 4].map((index) => (
+              <div
+                key={index}
+                className="flex items-center gap-4 p-2 bg-white rounded shadow"
+              >
+                <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+                <p className="text-lg font-semibold">Nome</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </aside>
+
+      {/* Main Content */}
+      <section className="w-3/4 p-6">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-semibold">Schedule a Cast</h1>
+          <a href="#" className="text-blue-500">
+            Analytics
           </a>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <div>
-        <DynamicWidget />
-        <NeynarAuthButton />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {user && (
-          <>
-            <div className="flex flex-col gap-4 w-96 p-4 rounded-md shadow-md">
-              <div className="flex items-center gap-4">
-                {user.pfp_url && (
-                  <Image
-                    src={user.pfp_url}
-                    width={40}
-                    height={40}
-                    alt="User Profile Picture"
-                    className="rounded-full"
-                  />
-                )}
-                <p className="text-lg font-semibold">{user?.display_name}</p>
-                <p className="text-lg font-semibold">{user?.fid}</p>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <div className="flex flex-col gap-4">
+            <textarea
+              className="w-full p-4 border border-gray-300 rounded"
+              placeholder="Cast text"
+              value={castText}
+              onChange={(e) => setCastText(e.target.value)}
+            ></textarea>
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="w-full border border-gray-300 rounded p-2"
+            />
+            <div className="flex gap-4">
+              <input
+                type="text"
+                value={publishTime}
+                onChange={(e) => setPublishTime(e.target.value)}
+                placeholder="Publish Time"
+                className="w-1/3 p-2 border border-gray-300 rounded"
+              />
+              <div className="w-1/3">
+                <label className="block text-sm font-medium text-gray-700">
+                  Publish Date
+                </label>
+                <input
+                  type="date"
+                  value={publishDate}
+                  onChange={(e) => setPublishDate(e.target.value)}
+                  className="input input-bordered w-full"
+                />
               </div>
+              <div className="w-1/3">
+                <label className="block text-sm font-medium text-gray-700">
+                  Publish Time
+                </label>
+                <input
+                  type="time"
+                  value={publishTime}
+                  onChange={(e) => setPublishTime(e.target.value)}
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <input
+                type="text"
+                value={channel}
+                onChange={(e) => setChannel(e.target.value)}
+                placeholder="Channel"
+                className="w-1/3 p-2 border border-gray-300 rounded"
+              />
             </div>
-          </>
-        )}
-      </div>
-      <div>
-        <input
-          type="text"
-          value={castText}
-          onChange={(e) => setCastText(e.target.value)}
-          placeholder="Enter cast text"
-        />
-        <input
-          type="text"
-          value={channel}
-          onChange={(e) => setChannel(e.target.value)}
-          placeholder="Channel"
-        />
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={handleCastClick} className="" disabled={isLoading}>
-          {isLoading ? "Scheduling..." : "Schedule cast"}
-        </button>
-      </div>
-      <a
-        href="https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRlex2yeMomsbkm0qzpHjtPf8j9QLCDPLZ_brREwaQIrpsnwot3sOfn8Qr3ujA92cho"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Github repo{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-          check the code
-        </p>
-      </a>
+            <button
+              onClick={handleCastClick}
+              className="bg-blue-500 text-white py-2 px-4 rounded"
+              disabled={isLoading}
+            >
+              {isLoading ? "Scheduling..." : "Schedule"}
+            </button>
+          </div>
+        </div>
+        <div className="mt-6 bg-white p-4 rounded shadow-md">
+          <p>Post cast txt</p>
+          <div className="flex justify-between mt-4">
+            <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+            <div className="flex items-center gap-2">
+              <p className="text-gray-600">DD/MM/YY</p>
+              <p className="text-gray-600">00:00</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
