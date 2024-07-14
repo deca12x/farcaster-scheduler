0b2c73ff2095847a0f881881b769f69c04cb4840
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-07-14 02:11:55 +0200

    getting analytics fetch ready

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -1,13 +1,33 @@
-const request = require("request");
+import { NextResponse } from "next/server";
 
-const options = {
-  method: "GET",
-  url: "https://api.neynar.com/v2/farcaster/feed?feed_type=filter&filter_type=fids&fids=410626&with_recasts=false&limit=100",
-  headers: { accept: "application/json", api_key: "NEYNAR_API_DOCS" },
-};
+export async function GET() {
+  const url =
+    "https://api.neynar.com/v2/farcaster/feed?feed_type=filter&filter_type=fids&fids=410626&with_recasts=false&limit=100";
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+  // Ensure that the API key is a string
+  const apiKey = process.env.NEYNAR_API_KEY ?? "";
 
-  console.log(body);
-});
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      api_key: apiKey,
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (err) {
+    console.error("error:", err);
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    } else {
+      return NextResponse.json(
+        { error: "An unknown error occurred" },
+        { status: 500 }
+      );
+    }
+  }
+}
